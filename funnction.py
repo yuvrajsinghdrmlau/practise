cb8f3e8b79e9701584747c2162eda47c2140df26
Yuvraj singh <146419722+yuvrajsinghdrmlau@users.noreply.github.com>
2026-02-21 18:49:51 +0530

    Add files via upload

---
+++ funnction.py
@@ -0,0 +1,27 @@
+
+
+def take_numbers():
+    l = []
+
+    while True:
+        a = input("give input until stop:")
+        if a =='stop':
+            break
+        
+        l.append(int(a))
+    return l
+        
+
+def calculate(l):
+    if len(l) == 0:
+        print("enter list numbers")
+        return
+    print("sum :",sum(l))
+    print("maximum number :",max(l))
+    average = (sum(l))/(len(l))
+    print("average: ", average)
+    print("minimum number:", min(l))
+
+exp = take_numbers()
+calculate(exp)
+
